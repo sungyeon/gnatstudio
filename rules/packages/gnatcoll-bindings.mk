52c66e9140996640addeea557f06872ef6386d70
sungyeon <sungyeon@hancom.com>
2020-12-07 23:42:19 +0900

    Update for python3.

--- rules/packages/gnatcoll-bindings.mk
+++ rules/packages/gnatcoll-bindings.mk
@@ -9,7 +9,7 @@ PYTHON_PREFIX    := $(shell python3-config --prefix)
 PYTHON_H_PATH    := $(PYTHON_PREFIX)/Headers
 PYTHON_LIB_PATH  := $(PYTHON_PREFIX)/lib
 
-$(PKG)_BINDING_LIST := zlib syslog readline omp iconv gmp python3
+$(PKG)_BINDING_LIST := zlib syslog readline omp iconv gmp
 define $(PKG)_BUILD_$(HOST)
     @echo Building $(1) package for $(HOST) host
     export C_INCLUDE_PATH=$(PYTHON_H_PATH) &&               \
@@ -17,4 +17,6 @@ define $(PKG)_BUILD_$(HOST)
     cd $(SRC_DIR)/$($(1)_SUBDIR) &&                         \
     for pkg in $($(1)_BINDING_LIST); do cd $(SRC_DIR)/$($(1)_SUBDIR)/$$pkg && ./setup.py build; done && \
     for pkg in $($(1)_BINDING_LIST); do cd $(SRC_DIR)/$($(1)_SUBDIR)/$$pkg && ./setup.py install; done
+    cd $(SRC_DIR)/$($(1)_SUBDIR)/python3 && ./setup.py build --python3-exec=python3 && \
+    ./setup.py install
 endef
